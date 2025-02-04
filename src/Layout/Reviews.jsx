efe617fd23e96b0a04ff571a42e223ad3c7e7734
nicoleables <159223166+nicoleables@users.noreply.github.com>
2025-02-03 20:11:51 -0600

    Update Reviews.jsx

--- src/Layout/Reviews.jsx
+++ src/Layout/Reviews.jsx
@@ -33,12 +33,12 @@ const Reviews = () => {
         </a>
       </div>
       <div className="review-slides">
-        <button className="arrow left-arrow" onClick={prevSlide}>←</button>
+        <button className="arrow left-arrow" onClick={prevSlide}>🡄</button>
         <div className="review-box">
           <div className="stars">⭐⭐⭐⭐⭐</div>
           <p>{reviews[currentSlide]}</p>
         </div>
-        <button className="arrow right-arrow" onClick={nextSlide}>→</button>
+        <button className="arrow right-arrow" onClick={nextSlide}>🡆</button>
       </div>
       <div className="leavereviewbox">
         <div className="review-container">
