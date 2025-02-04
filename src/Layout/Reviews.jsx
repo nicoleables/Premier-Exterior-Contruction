b0f3b03841f8130cc6997eee8f3f18d176d2a56c
nicoleables <159223166+nicoleables@users.noreply.github.com>
2025-02-03 20:13:49 -0600

    Update Reviews.jsx

--- src/Layout/Reviews.jsx
+++ src/Layout/Reviews.jsx
@@ -38,7 +38,7 @@ const Reviews = () => {
           <div className="stars">⭐⭐⭐⭐⭐</div>
           <p>{reviews[currentSlide]}</p>
         </div>
-        <button className="arrow right-arrow" onClick={nextSlide}>🡆</button>
+        <button className="arrow right-arrow" onClick={nextSlide}>🡺</button>
       </div>
       <div className="leavereviewbox">
         <div className="review-container">
