0dbb4e0792fa4474a22cb26c6893d5242e2de356
nicoleables <159223166+nicoleables@users.noreply.github.com>
2026-03-29 21:28:26 -0500

    Update Home.jsx

--- src/Layout/Home.jsx
+++ src/Layout/Home.jsx
@@ -1,5 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
+import { Helmet } from "react-helmet"; // <-- import Helmet
 import "../styles/home.css";
 import "../styles/global.css";
 import coverimage from "../assets/home/images/coverphoto.jpg";
@@ -7,10 +8,53 @@ import coverimage from "../assets/home/images/coverphoto.jpg";
 const Home = () => {
   return (
     <div className="home">
-        <img className="background-image" src={coverimage} alt="Background Image" />
-        <div className="overlay">
+      {/* Helmet for SEO */}
+      <Helmet>
+        <title>Premier Exterior Construction | Goldthwaite, TX</title>
+        <meta
+          name="description"
+          content="Premier Exterior Construction provides carports, shops, barndominiums, roofing, siding, windows, welding, and fencing in Goldthwaite, TX, Austin, Fort Worth, Arlington, Killeen, Waco, Abilene, Round Rock, San Angelo, and Temple."
+        />
+        <script type="application/ld+json">
+          {`
+            {
+              "@context": "https://schema.org",
+              "@type": "LocalBusiness",
+              "name": "Premier Exterior Construction",
+              "image": "https://yourdomain.com/logo.png",
+              "address": {
+                "@type": "PostalAddress",
+                "streetAddress": "Your Street Address",
+                "addressLocality": "Goldthwaite",
+                "addressRegion": "TX",
+                "postalCode": "76844",
+                "addressCountry": "US"
+              },
+              "telephone": "123-456-7890",
+              "areaServed": [
+                "Goldthwaite, TX",
+                "Austin, TX",
+                "Fort Worth, TX",
+                "Arlington, TX",
+                "Killeen, TX",
+                "Waco, TX",
+                "Abilene, TX",
+                "Round Rock, TX",
+                "San Angelo, TX",
+                "Temple, TX"
+              ],
+              "url": "https://yourdomain.com",
+              "priceRange": "$$"
+            }
+          `}
+        </script>
+      </Helmet>
+
+      {/* Page content */}
+      <img className="background-image" src={coverimage} alt="Background Image" />
+      <div className="overlay">
         <div className="overlay-text-container">
-        <div className="overlay-text">
+          <div className="overlay-text">
             <span>PREMIER</span>
             <span>EXTERIOR</span>
             <span>CONSTRUCTION</span>
