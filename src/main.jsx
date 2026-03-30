4a87e0a23e64c623c9df79ebb873c3a1b407151d
nicoleables <159223166+nicoleables@users.noreply.github.com>
2026-03-29 21:39:39 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
-// eslint-disable-next-line no-unused-vars
-import React from "react";
-import { createRoot } from "react-dom/client";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
-
