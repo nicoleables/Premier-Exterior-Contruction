56158316063d5a72f7c1120347250fe833e5e262
nicoleables <159223166+nicoleables@users.noreply.github.com>
2026-03-29 21:44:47 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
-import { defineConfig } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
-    server: {
-    port: 3003,
+  plugins: [react()],
+  server: {
+    port: 3003, // your custom port
     open: true,
-  },
-  alias: {
-    'react-bootstrap': 'react-bootstrap/dist/react-bootstrap.esm.min.js'
   },
 });
